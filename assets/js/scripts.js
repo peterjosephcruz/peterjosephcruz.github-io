e79b83ccc807f048ceb7fd575cb6953823b02c31
testg <test@test.com>
2017-08-17 08:15:01 -0700

    Update Send via Mail :D

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -201,24 +201,10 @@ jQuery(function ($) {
         removalDelay: 300, // Delay in milliseconds before popup is removed
         mainClass: 'mfp-with-zoom', // this class is for CSS animation below
         type:'image'
+
       });
 
     }());
-
-
-
-    (function () {
-        $('.popup-video').magnificPopup({
-            disableOn: 700,
-            type: 'iframe',
-            mainClass: 'mfp-with-zoom',
-            removalDelay: 300,
-            preloader: false,
-            fixedContentPos: false
-        });
-    }());
-
-
 
 
 
@@ -285,42 +271,6 @@ jQuery(function ($) {
         }).init();
 
     }());
-
-
-
-    // -------------------------------------------------------------
-    // Contact Form
-    // -------------------------------------------------------------
-
-    $('#contactForm').on('submit',function(e){
-
-        e.preventDefault();
-
-        var $action = $(this).prop('action');
-        var $data = $(this).serialize();
-        var $this = $(this);
-
-        $this.prevAll('.alert').remove();
-
-        $.post( $action, $data, function( data ) {
-
-            if( data.response=='error' ){
-
-                $this.before( '<div class="alert alert-danger">'+data.message+'</div>' );
-            }
-
-            if( data.response=='success' ){
-
-                $this.before( '<div class="alert alert-success">'+data.message+'</div>' );
-                $this.find('input, textarea').val('');
-            }
-
-        }, "json");
-
-    });
-
-
-
 
     // -------------------------------------------------------------
     // Google Map
